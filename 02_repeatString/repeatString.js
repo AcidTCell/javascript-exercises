8aba02563888aa30a759f6bda13f85fb73128ade
AcidTCell <cimafrancajasper.personal@gmail.com>
2023-12-02 02:58:33 +0800

    Finalize repeat string js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(x, y) {
+    let originalString = '';
+    let repeatNum = y;
+    if (y < 0){
+        return "ERROR";
+    }
+    while (repeatNum > 0){
+        originalString = originalString += x;
+        repeatNum--;
+    }
+    return originalString;
 
 };
 
